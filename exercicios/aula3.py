82db8f836b8a7ecfc164ee373d7f90b8f8047cbb
Jessica Ferreira <jessiberteloni@gmail.com>
2022-07-25 18:40:40 -0300

    Strings e aspas

---
+++ exercicios/aula3.py
@@ -0,0 +1,18 @@
+"""
+str-string
+"""
+
+print('alguma coisa')
+print("Aspas duplas")
+print(123456)  #int numeros inteiros
+
+print("Essa é uma 'string' (str).")  # Na programação, tudo que abre fecha, ex: "" () {}
+print('Essa é uma "string" (str).')
+print("Essa é uma \"string\" (str).")  # Forma errada
+print("Esse é um \"texto\" (str).")  # Essa forma não funciona
+print('Esse é um \'texto\' (str).')  # \escape ele anula as aspas iguais
+print('Essa é uma  \n (str).' )  #  \n quebra de linha
+print(r'Essa é uma  \n (str).' )  # r antes das aspas mostar o codigo inteiro
+print(r'Esse é um \'Aspas aqui\' (str).')  # r na frente mostra o codigo inteiro com as aspas no meio, modo errado
+
+
