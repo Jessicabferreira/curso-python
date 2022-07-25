3149b44e488294c32dd6a63a847a85eafbcdb9e7
Jessica Ferreira <jessiberteloni@gmail.com>
2022-07-25 18:38:00 -0300

    Comando print

---
+++ exercicios/aula2.py
@@ -0,0 +1,9 @@
+print(123456)
+print('Jessica', 'Ferreira')  # virgula fora das aspas da espaço
+print('Jessy, Ferreira')  # virgula dentro das aspas é virgula mesmo
+print('Jessica', 'e', 'Pedro', sep='-', end='#####')  # SEP='' separador
+print('Jessica', 'e', 'Pedro', sep='-')  # end='' quebra de linha
+
+#  print(452.567.089-18)
+print('452', '567', '000', sep='.', end='-')
+print('18')
