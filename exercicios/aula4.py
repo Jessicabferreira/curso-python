5a2590ebcff5d472d5ac6b2902b53c44f816222f
Jessica Ferreira <jessiberteloni@gmail.com>
2022-07-25 18:44:54 -0300

    Tipos de dados primitivos

---
+++ exercicios/aula4.py
@@ -0,0 +1,30 @@
+"""
+Tipos de dados
+str - string - textos 'Assim' "Assim"
+int - inteiro- 123456 0 -10 -20 -50 -60 -15000 1500
+float - real/ponto flutuante - 10.50 1.5 -10.10 -50.93 0.0
+bool - booleano/logico - true/False 10 == 10
+"""
+
+print('Jessica', type('Jessica'))
+print(10, type(10))
+print(25.23, type(25.23))
+print(10 == 10, type(10 == 10))
+print('Jessica', type('Jessica'), bool('Jessica'))
+
+print('10'+ '10') # juntou, pq está dentro de aspas/ como se fosse texto
+print(10+10) # somou como int
+print('Jessica', (' '))
+
+#String: nome
+print("Jessica",type ("Jessica"))
+
+#Idade: int
+print(26,type(26))
+
+#Altura:float
+print(1.56,type(1.56))
+
+#É maior de idade 10 > 20
+print(26 > 18, type(26 > 18))
+
