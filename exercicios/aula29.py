7f686e9236b94b457ba8040c4fde99b31857db34
Jessica Ferreira <jessiberteloni@gmail.com>
2022-09-14 19:03:17 -0300

    Trocando o valor entre variáveis em python

---
+++ exercicios/aula29.py
@@ -0,0 +1,15 @@
+# Exemplo 1
+x = 10         # Jessica
+y = 'Jessica'  # 10
+x, y = y, x    # varial invertida
+
+print(f'x={x} e y={y}')
+print()
+
+# Exemplo 2
+x = 10         # Pedro
+y = 'Jessica'  # 10
+z = 'Pedro'    # Jessica
+x, y, z = z, x, y
+
+print(f'x={x} e y={y} e z={z}')
